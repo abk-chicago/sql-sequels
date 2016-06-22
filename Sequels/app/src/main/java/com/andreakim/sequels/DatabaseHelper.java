7b202e1cbe3d15faadf2e914795d02c6f932e4f7
Andrea Kim <andreaforanimals@gmail.com>
2016-06-22 11:12:58 -0500

    projects started

---
+++ Sequels/app/src/main/java/com/andreakim/sequels/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.andreakim.sequels;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by andreakim on 6/22/16.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DB_VERSION = 1;   //<-- all caps syntax is onl for finals, very important; change as we upgrade
+    public static final String DB_NAME = "ReallyBadSequels.db";  // name of your .db
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
