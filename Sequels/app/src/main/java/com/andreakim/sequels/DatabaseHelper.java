55ea33ddc92b28c36c37e9c7326ac8be93e8a5fb
Andrea Kim <andreaforanimals@gmail.com>
2016-06-22 11:31:53 -0500

    working on seeding the db

--- Sequels/app/src/main/java/com/andreakim/sequels/DatabaseHelper.java
+++ Sequels/app/src/main/java/com/andreakim/sequels/DatabaseHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DB_VERSION = 1;   //<-- all caps syntax is onl for finals, very important; change as we upgrade
+    public static final int DB_VERSION = 1;   // all caps syntax is only for finals, very important; change as we upgrade
     public static final String DB_NAME = "ReallyBadSequels.db";  // name of your .db
     public static final String CREATE_TABLE = "CREATE TABLE sequels (id INT PRIMARY KEY, name TEXT);";
     public static final String DROP_TABLE = "DROP TABLE IF EXISTS sequels;";
@@ -22,11 +22,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE );  //create table
     }
+
+    // database name (i.e. db) can be anything
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    
+        db.execSQL(DROP_TABLE);  //drop table
+        onCreate(db);  //after erasing it, want to re-seed the database, re-create the table
     }
+
+    // NOT an @Override
+    public void seedDatabase() {
+
+    }
+
+    public void insertSequel(int id, String name) {
+
+    }
+
 }
